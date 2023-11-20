0e0b31766ca2b45f0f4dc6eacc5b990d89e5d5c8
developer_pomu <103820259+pomuCH@users.noreply.github.com>
2023-11-20 16:43:22 +0900

    素因数分解.py

--- python/素因数分解.py
+++ python/素因数分解.py
@@ -1,13 +1,13 @@
 n =5487962238152
-lis=[]
+s=[]
 
 for i in range(2,n+1):
     while True:
         if n%i == 0 and n!=1:
-            lis.append(i) # 余り0なら素因数分解リストにappendする
-            n = n//i # nの更新
-            print(lis)
-            print("n={}".format(n)) # nの更新状況をみてみる
+            s.append(i)
+            n = n//i
+            print(s)
+            print("n={}".format(n))
         elif n==1:
             break
         else:
