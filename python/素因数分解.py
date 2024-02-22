34be860b54a6ee662c22b56bc95ab16af5edb4cd
developer_pomu <103820259+pomuCH@users.noreply.github.com>
2024-02-22 12:58:45 +0900

    Update 素因数分解.py

--- python/素因数分解.py
+++ python/素因数分解.py
@@ -2,13 +2,13 @@ n = 30011
 s=[]
 
 for i in range(2,n+1):
-    while True:
-        if n%i == 0 and n!=1:
-            s.append(i)
-            n = n//i
-            print(s)
-            print("n={}".format(n))
-        elif n==1:
-            break
-        else:
-            break
+  while True:
+    if n%i == 0 and n!=1:
+      s.append(i)
+      n = n//i
+      print(s)
+      print("n={}".format(n))
+    elif n==1:
+      break
+    else:
+      break
